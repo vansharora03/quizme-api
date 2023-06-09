bb1afa25401a308a6d8dc2865bf4dc011d6d1439
Dhruv Patel <dhruv225@outlook.com>
2023-06-08 22:51:48 -0400

    Add check for if expected body is returned

--- cmd/api/testutils_test.go
+++ cmd/api/testutils_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -53,12 +54,18 @@ func TestServerListening(t *testing.T) {
 	defer ts.Close()
 
 	// Make a GET request to the health check endpoint
-	_, statusCode, _ := ts.GET(t, "/v1/healthcheck")
+	_, statusCode, body := ts.GET(t, "/v1/healthcheck")
 
 	// Check if the server is listening and returns the expected status code
 	expectedStatus := http.StatusOK
 	if statusCode != expectedStatus {
-		t.Errorf("Expected status code %d, but got %d", expectedStatusCode, statusCode)
+		t.Errorf("Expected status code %d, but got %d", expectedStatus, statusCode)
+	}
+
+	// Check if the server is listening and returns the expected body
+	expectedBody := fmt.Sprintf("Environment: development\nVersion: %s", version)
+	if expectedBody != string(body) {
+		t.Errorf("Expected body %s, but got %s", expectedBody, string(body))
 	}
 
 }
