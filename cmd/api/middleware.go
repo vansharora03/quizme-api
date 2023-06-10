9a513a200f006d4908691e0279e3b34691b2897f
Dhruv Patel <dhruv225@outlook.com>
2023-06-10 02:01:17 -0400

    Create secureHeaders to set security headers

---
+++ cmd/api/middleware.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+)
+
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set security headers
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		w.Header().Set("X-Frame-Options", "deny")
+
+		// Call the next handler
+		next.ServeHTTP(w, r)
+	})
+}
