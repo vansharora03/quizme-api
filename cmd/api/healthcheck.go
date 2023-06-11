4119953305c6c8f5b79ce5aaedb5e0b0ec73b746
vansharora03 <vansh1993@icloud.com>
2023-06-11 18:25:35 -0400

    Extract healthcheck handler into separate file, and send as json

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+// healthcheckHandler sends a json response representing the status
+// of the application.
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{}
+	response["environment"] = "development"
+	response["version"] = "1.0.0"
+	err := app.writeJSON(w, r, http.StatusOK, response, nil)
+	if err != nil {
+		app.logger.Println(err)
+	}
+}
