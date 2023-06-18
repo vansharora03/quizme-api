69b2f20a06394b2786b0af249a5b13ebdb911242
Vansh Arora <vansh94arora@gmail.com>
2023-06-18 18:44:33 -0400

    Change foreign key type to bigint from bigserial

--- sql-setup/setup/question_table.sql
+++ sql-setup/setup/question_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS question (
     id bigserial PRIMARY KEY,
-    quiz_id bigserial REFERENCES quiz(id),
+    quiz_id bigint REFERENCES quiz(id),
     prompt text NOT NULL,
     choices text[] NOT NULL,
     correct_index integer NOT NULL,
