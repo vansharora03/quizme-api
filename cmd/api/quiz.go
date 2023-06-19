3ae6ee351c3c636a20cd2cf33babbcd64245c8d0
Vansh Arora <vansh94arora@gmail.com>
2023-06-19 00:21:06 -0400

    Add implementation and test for showing specific quiz handler stub

--- cmd/api/quiz.go
+++ cmd/api/quiz.go
@@ -8,4 +8,10 @@ func (app *application) showAllQuizzesHandler(w http.ResponseWriter, r *http.Req
     app.writeJSON(w, r, http.StatusOK, "Showing all quizzes...", nil)
 }
 
+// showQuizHandler sends a specific quiz in the database in a JSON response to the
+// client
+func (app *application) showQuizHandler(w http.ResponseWriter, r *http.Request) {
+    app.writeJSON(w, r, http.StatusOK, "Showing specific quiz...", nil)
+}
+
 
--- cmd/api/quiz_test.go
+++ cmd/api/quiz_test.go
@@ -22,3 +22,20 @@ func TestShowAllQuizzesHandler(t *testing.T) {
         t.Fatalf("INCORRECT BODY: expected %q, got %q", expectedBody, body)
     }
 }
+
+func TestShowQuizHandler(t *testing.T) {
+    ts := newTestServer(t)
+    defer ts.Close()
+    _, code, body := testGET[string](t, ts, "/v1/quiz/1")
+
+    expectedCode := http.StatusOK
+    expectedBody := "Showing specific quiz..."
+
+    if code != expectedCode {
+        t.Fatalf("INCORRECT STATUS CODE: expected %d, got %d", expectedCode, code)
+    }
+
+    if !reflect.DeepEqual(body, expectedBody) {
+        t.Fatalf("INCORRECT BODY: expected %q, got %q", expectedBody, body)
+    }
+}
