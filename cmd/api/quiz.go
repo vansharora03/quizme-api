53dae1d3403bd75ffea4c938de29f42e52fe02d0
Vansh Arora <101963633+vansharora03@users.noreply.github.com>
2023-06-19 13:11:18 -0400

    Revert "Revert "Feature/show all quizzes handler""

--- cmd/api/quiz.go
+++ cmd/api/quiz.go
@@ -1,11 +1,18 @@
 package main
 
 import "net/http"
+import _ "vanshadhruvp/quizme-api/internal/data"
 
 // showAllQuizzesHandler sends all quizzes in the database in a JSON response to the
 // client
 func (app *application) showAllQuizzesHandler(w http.ResponseWriter, r *http.Request) {
-    app.writeJSON(w, r, http.StatusOK, "Showing all quizzes...", nil)
+    quizzes, err := app.models.Quizzes.GetAll()
+    if err != nil {
+        app.serverErrorResponse(w, r, err)
+        return
+    }
+
+    app.writeJSON(w, r, http.StatusOK, quizzes, nil)
 }
 
 // showQuizHandler sends a specific quiz in the database in a JSON response to the
--- internal/data/models.go
+++ internal/data/models.go
@@ -3,15 +3,15 @@ package data
 import "database/sql"
 
 type Models struct {
-    questions  QuestionModel
-    quizzes    QuizModel
+    Questions  interface{}
+    Quizzes    interface{ GetAll() ([]*Quiz, error) }
 }
 
 // NewModels initializes a Models struct with
 // all of quizme's data models connected on db
 func NewModels(db *sql.DB) Models {
     return Models{
-        QuestionModel{db},
-        QuizModel{db},
+        Questions: QuestionModel{db},
+        Quizzes: QuizModel{db},
     }
 }
