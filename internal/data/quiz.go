2736f04c4dc022f20c2b526779e9a51e16044870
Dhruv Patel <dhruv225@outlook.com>
2023-06-20 02:57:13 -0400

    Create Get method to get a specifed quiz

--- internal/data/quiz.go
+++ internal/data/quiz.go
@@ -7,46 +7,71 @@ import (
 )
 
 type QuizModel struct {
-    DB *sql.DB
+	DB *sql.DB
 }
 
 type Quiz struct {
-    ID int64 // ID
-    CreatedAt time.Time // When the quiz was created
-    Version int32 // Version of the quiz
-    Title string // Title of the quiz
+	ID        int64     // ID
+	CreatedAt time.Time // When the quiz was created
+	Version   int32     // Version of the quiz
+	Title     string    // Title of the quiz
 }
-
 
 // GetAll will get all quizzes in the database and return them.
 func (m QuizModel) GetAll() ([]*Quiz, error) {
-    stmt := `SELECT id, created_at, version, title FROM quiz`
-    ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
-    defer cancel()
+	stmt := `SELECT id, created_at, version, title FROM quiz`
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-    rows, err := m.DB.QueryContext(ctx, stmt)
-    if err != nil {
-        return nil, err
-    }
-    defer rows.Close()
+	rows, err := m.DB.QueryContext(ctx, stmt)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
 
-    quizzes := []*Quiz{}
+	quizzes := []*Quiz{}
 
-    for rows.Next() {
-        q := &Quiz{}
-        err := rows.Scan(&q.ID, &q.CreatedAt, &q.Version, &q.Title)
-        if err != nil {
-            return nil, err
-        }
+	for rows.Next() {
+		q := &Quiz{}
+		err := rows.Scan(&q.ID, &q.CreatedAt, &q.Version, &q.Title)
+		if err != nil {
+			return nil, err
+		}
 
-        quizzes = append(quizzes, q)
-    }
+		quizzes = append(quizzes, q)
+	}
 
-    err = rows.Err()
+	err = rows.Err()
 
-    if err != nil {
-        return nil, err
-    }
+	if err != nil {
+		return nil, err
+	}
 
-    return quizzes, nil
+	return quizzes, nil
+}
+
+// Get will get a specific quiz from the database and return it based on the id.
+func (m QuizModel) Get(id string) (*Quiz, error) {
+
+	stmt := `SELECT id, created_at, version, title FROM quiz WHERE id = $1`
+
+	// Create a context with a timeout of 5 seconds
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Query the database for the quiz
+	row := m.DB.QueryRowContext(ctx, stmt, id)
+
+	// Create a new Quiz struct
+	q := &Quiz{}
+
+	// Copy the values from row into the Quiz struct
+	err := row.Scan(&q.ID, &q.CreatedAt, &q.Version, &q.Title)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the quiz
+	return q, nil
 }
