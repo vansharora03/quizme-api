58bdb4ee884605b46baaaa0fefd61656c15929fc
Dhruv Patel <dhruv225@outlook.com>
2023-06-20 02:58:09 -0400

    Create test-util for Get.

--- cmd/api/testutils_test.go
+++ cmd/api/testutils_test.go
@@ -19,36 +19,49 @@ type testServer struct {
 }
 
 // Mocks QuizModel
-type TestQuizModel struct {}
+type TestQuizModel struct{}
 
 // test quiz data
-var quiz1 *data.Quiz =  &data.Quiz{
-        Title: "quiz1",
-        Version: 4,
-        ID: 1,
-        CreatedAt: time.Date(2005, time.April, 5, 1, 2, 3, 0, time.UTC),
-    }
+var quiz1 *data.Quiz = &data.Quiz{
+	Title:     "quiz1",
+	Version:   4,
+	ID:        1,
+	CreatedAt: time.Date(2005, time.April, 5, 1, 2, 3, 0, time.UTC),
+}
 var quiz2 *data.Quiz = &data.Quiz{
-        Title: "quiz2",
-        Version: 1,
-        ID: 2,
-        CreatedAt: time.Date(2020, time.March, 20, 0, 0, 5, 0, time.UTC),
-    }
+	Title:     "quiz2",
+	Version:   1,
+	ID:        2,
+	CreatedAt: time.Date(2020, time.March, 20, 0, 0, 5, 0, time.UTC),
+}
 
 // Mocks data.QuizModel.GetAll to test showAllQuizzesHandler
 func (m TestQuizModel) GetAll() ([]*data.Quiz, error) {
-    return []*data.Quiz{quiz1, quiz2}, nil
+	return []*data.Quiz{quiz1, quiz2}, nil
+}
+
+// Mocks data.QuizModel.Get to test showQuizHandler
+func (m TestQuizModel) Get(id string) (*data.Quiz, error) {
+	// If id is 1, return quiz1
+	if id == "1" {
+		return quiz1, nil
+		// If id is 2, return quiz2
+	} else if id == "2" {
+		return quiz2, nil
+
+	}
+	return nil, fmt.Errorf("Quiz not found")
 }
 
 // Mocks QuestionModel
-type TestQuestionModel struct {}
+type TestQuestionModel struct{}
 
 // Create mock Models
 func newTestModel(t *testing.T) data.Models {
-    return data.Models{
-        Quizzes: TestQuizModel{},
-        Questions: TestQuestionModel{},
-    }
+	return data.Models{
+		Quizzes:   TestQuizModel{},
+		Questions: TestQuestionModel{},
+	}
 }
 
 func newTestApp(t *testing.T) *application {
@@ -62,8 +75,8 @@ func newTestApp(t *testing.T) *application {
 		config: cfg,
 		// Create a new logger with the buffer
 		logger: log.New(bytes.NewBuffer(buffer), "", log.Default().Flags()),
-        // Pass in mock models
-        models: newTestModel(t),
+		// Pass in mock models
+		models: newTestModel(t),
 	}
 
 	return testApp
@@ -95,40 +108,39 @@ func testGET[T any](t *testing.T, ts *testServer, urlPath string) (http.Header, 
 
 	defer rs.Body.Close()
 
-    return openResponse[T](t, ts, rs, urlPath)
+	return openResponse[T](t, ts, rs, urlPath)
 
 }
 
-//testPOST performs a post request on ts. Returns headers,
+// testPOST performs a post request on ts. Returns headers,
 // status code, and body. Initialize the generic to be the type of the json response
 // once converted to a Go value.
 func testPOST[T any](t *testing.T, ts *testServer, urlPath string) (http.Header, int, T) {
-    // Make request
-    rs, err := ts.Client().Post(ts.URL + urlPath, "application/json", bytes.NewBuffer([]byte{}))
-    // Fail test on error
-    check(t, err)
+	// Make request
+	rs, err := ts.Client().Post(ts.URL+urlPath, "application/json", bytes.NewBuffer([]byte{}))
+	// Fail test on error
+	check(t, err)
 
-    defer rs.Body.Close()
+	defer rs.Body.Close()
 
-    return openResponse[T](t, ts, rs, urlPath)
+	return openResponse[T](t, ts, rs, urlPath)
 }
-
 
 // openResponse takes the rs response that contains json data, and attempts to extract
 // the headers, status code, and body as a Go value of type T.
 func openResponse[T any](t *testing.T, ts *testServer, rs *http.Response, urlPath string) (http.Header, int, T) {
 
-    // Extract body
-    body, err := io.ReadAll(rs.Body)
-    // Fail test on error
-    check(t, err)
-    var dst T
-    rr := httptest.NewRecorder()
-    r, err := http.NewRequest("GET", urlPath, bytes.NewBuffer(body))
-    check(t, err)
-    err = ts.app.readJSON(rr, r, &dst)
-    check(t, err)
+	// Extract body
+	body, err := io.ReadAll(rs.Body)
+	// Fail test on error
+	check(t, err)
+	var dst T
+	rr := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", urlPath, bytes.NewBuffer(body))
+	check(t, err)
+	err = ts.app.readJSON(rr, r, &dst)
+	check(t, err)
 
-    return rs.Header, rs.StatusCode, dst
+	return rs.Header, rs.StatusCode, dst
 
 }
