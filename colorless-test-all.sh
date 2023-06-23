a26510b0658276ce7d32d5a8367a9416d0dca270
Vansh Arora <vansh94arora@gmail.com>
2023-06-22 21:10:57 -0400

    Do not allow test caching during test script run

--- colorless-test-all.sh
+++ colorless-test-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./sql-test-setup.sh
-go test ./... -v -cover
+go test ./... -v -cover -count=1
 ./sql-test-teardown.sh
--- test-all.sh
+++ test-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./sql-test-setup.sh
-grc go test ./... -v -cover
+grc go test ./... -v -cover count=1
 ./sql-test-teardown.sh
