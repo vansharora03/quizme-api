d1363ec70381a1c5b8bfb7cc4654158dba067698
Dhruv Patel <dhruv225@outlook.com>
2023-06-23 00:49:13 -0400

    Create validate question

--- internal/validator/validate.go
+++ internal/validator/validate.go
@@ -10,6 +10,18 @@ func ValidateQuiz(quiz data.Quiz) error {
 	return nil
 }
 
+func ValidateQuestion(question data.Question) error {
+	if question.Prompt == "" {
+		return NewValidationError("Prompt is required")
+	} else if question.Choices == nil {
+		return NewValidationError("Choices are required")
+	} else if question.CorrectIndex < 0 || question.CorrectIndex >= int32(len(question.Choices)) {
+		return NewValidationError("Correct index is required")
+	}
+	return nil
+
+}
+
 type ValidationError struct {
 	ErrorMessage string
 }
