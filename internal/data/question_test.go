5144d0f49b67d40cb42bb0b08fa15872d00d4c4a
Vansh Arora <vansh94arora@gmail.com>
2023-06-25 23:37:27 -0400

    Add test for AddQuestion

--- internal/data/question_test.go
+++ internal/data/question_test.go
@@ -1,6 +1,13 @@
 package data
 
-import "testing"
+import (
+	"context"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/lib/pq"
+)
 
 
 func TestGetAllByQuizID(t *testing.T) {
@@ -15,6 +22,73 @@ func TestGetAllByQuizID(t *testing.T) {
     if len(questions) != 2 {
         t.Fatalf("INCORRECT ENTRY LENGTH: expected %d, got %d", 2, len(questions))
     }
+
+
+}
+
+
+func TestAddQuestion(t *testing.T) {
+    questionModel := QuestionModel{openTestDB(t)}
+
+    tests := []struct{
+        name string
+        prompt string
+        choices []string
+        correctIndex int32
+        quizID int64
+        expectedError error
+    }{{"Existent quizID", "prompt123", []string{"A", "Z", "Q"}, 2, 1, nil},
+      {"Nonexistent quizID", "prompt123", []string{"A", "Z", "Q"}, 2, 3, ErrNoRecords},
+    }
+
+    for _, tt := range tests {
+        t.Run(tt.name, func(t *testing.T) {
+            question := Question{
+                Prompt: tt.prompt,
+                Choices: tt.choices,
+                CorrectIndex: tt.correctIndex,
+                QuizID: tt.quizID,
+            }
+
+            err := questionModel.AddQuestion(&question)
+            if err != nil {
+                if err != tt.expectedError {
+                    t.Fatalf("INCORRECT ERROR: expected %q, got %q", tt.expectedError, err)
+                }
+                return
+            }
+
+            // Query should return the same question 
+            stmt := `SELECT id, created_at, version, prompt, choices, correct_index, quiz_id
+            FROM question WHERE id = $1`
+
+            ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+            defer cancel()
+
+            row := questionModel.DB.QueryRowContext(ctx, stmt, question.ID)
+            dupeQuestion := Question{}
+
+            err = row.Scan(&dupeQuestion.ID,
+                &dupeQuestion.CreatedAt,
+                &dupeQuestion.Version,
+                &dupeQuestion.Prompt,
+                pq.Array(&dupeQuestion.Choices),
+                &dupeQuestion.CorrectIndex,
+                &dupeQuestion.QuizID)
+
+            if err != nil {
+                t.Fatal(err)
+            }
+
+            if !reflect.DeepEqual(question, dupeQuestion) {
+                t.Fatalf("INCORRECT ENTRY: expected %v, got %v", question, dupeQuestion)
+            }
+
+        })
+
+    }
+
+
 
 
 }
