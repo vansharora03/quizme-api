57efa0c23c62916ff6b02550a3134c11dc68fa27
Vansh Arora <vansh94arora@gmail.com>
2023-06-26 21:20:55 -0400

    Added score functionality

--- cmd/api/quiz.go
+++ cmd/api/quiz.go
@@ -91,12 +91,6 @@ func (app *application) addQuizHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// addScoreHandler receives a json response containing the user's
-// quiz answers and returns the user's score on the quiz.
-func (app *application) addScoreHandler(w http.ResponseWriter, r *http.Request) {
-	app.writeJSON(w, r, http.StatusOK, "Adding a score...", nil)
-}
-
 // addQuestionHandler receives a json response of a question and
 // adds the question to the database, as well as responding to the
 // client with the added question.
@@ -146,4 +140,40 @@ func (app *application) addQuestionHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	app.writeJSON(w, r, http.StatusCreated, question, nil)
+}
+
+// addScoreHandler receives a json response containing the user's
+// quiz answers and returns the user's score on the quiz.
+func (app *application) addScoreHandler(w http.ResponseWriter, r *http.Request) {
+    params := httprouter.ParamsFromContext(r.Context())
+    id := params.ByName("id")
+    var input struct {
+        Answers []int32
+    }
+
+    err := app.readJSON(w, r, &input)
+    if err != nil {
+        app.errorResponse(w, r, http.StatusBadRequest, err)
+        return
+    }
+
+    questions, err := app.models.Questions.GetAllByQuizID(id)
+    if err != nil {
+        app.serverErrorResponse(w, r, err)
+        return
+    }
+
+    correct := 0
+    for i, question := range questions {
+        if input.Answers[i] == question.CorrectIndex {
+            correct += 1
+        }
+    }
+
+    score := float32(correct) / float32(len(questions)) * 100.0
+
+    err = app.writeJSON(w, r, http.StatusOK, score, nil)
+    if err != nil {
+        app.serverErrorResponse(w, r, err)
+    }
 }
