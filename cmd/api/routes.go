dd39bc89c76d20fb5fef5b6c954ee8edf4720fc8
Vansh Arora <vansh94arora@gmail.com>
2023-06-29 23:19:17 -0400

    Add update quiz route

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -28,6 +28,8 @@ func (app *application) routes() http.Handler {
     router.HandlerFunc(http.MethodPost, "/v1/quiz", app.addQuizHandler)
     router.HandlerFunc(http.MethodPost, "/v1/quiz/:id/score", app.addScoreHandler)
     router.HandlerFunc(http.MethodPost, "/v1/quiz/:id/question", app.addQuestionHandler)
+    router.HandlerFunc(http.MethodPut, "/v1/quiz/:id", app.updateQuizHandler)
+
 
 	return standardMiddleware.Then(router)
 }
